c12cc3e1cc2e8583960f9c9c4510b230966c5929
st2l <dolka007st@gmail.com>
2024-12-12 19:09:40 +0300

    index is DONZO!!!

--- routes/index.py
+++ routes/index.py
@@ -1,6 +1,18 @@
 from flask import render_template
 from helpers import filter_params
+from models import Category, Lamp
 
 
 def index():
-    return render_template('index.html', **filter_params())
+    park_lights_idx = Category.query.filter(Category.name.contains("Парковые фонари")).first()
+    expensive_lamps = Lamp.query.order_by(Lamp.price.desc()).limit(15).all()
+
+    params = {
+        'park_lights_idx': park_lights_idx.id,
+        'lamps': expensive_lamps,
+        
+    }
+
+    params.update(filter_params())
+
+    return render_template('index.html', **params)
